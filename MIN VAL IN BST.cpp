21580d86cec94b06280138b5007717a7e594f5bb
ARIF AKHTAR <120799230+arifakhtar51@users.noreply.github.com>
2023-06-26 02:36:48 +0530

    Update and rename MIN VAL IN BST to MIN VAL IN BST.cpp

--- MIN VAL IN BST.cpp
+++ MIN VAL IN BST.cpp
@@ -10,3 +10,17 @@ int minValue(Node* root) {
     solve(root,ans);
     return ans;
 }
+
+
+
+//OPTIMIZE
+int minValue(Node* root) {
+    // Code here
+    if(root==0)return -1;
+    Node *temp=root;
+    while(root){
+        temp=root;
+        root=root->left;
+    }
+    return temp->data;
+}
